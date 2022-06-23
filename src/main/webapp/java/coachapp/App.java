a00c1745c8fdf3d4ad0c09e5772b1955c54ce09a
Alina Mlynorik <alina.sudnick.@gmail.com>
2022-06-23 20:09:43 +0300

    coachapp-update-5: bean-scope-prototype

--- src/main/webapp/java/coachapp/App.java
+++ src/main/webapp/java/coachapp/App.java
@@ -4,11 +4,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        Coach coach;
+        Coach coach, alphaCoach;
         try (ClassPathXmlApplicationContext applicationContext =
-                     new ClassPathXmlApplicationContext("coachappConfig.xml")) {
+                     new ClassPathXmlApplicationContext("coachappConfig-beanScope.xml")) {
             coach = applicationContext.getBean("coach", Coach.class);
+            alphaCoach = applicationContext.getBean("coach", Coach.class);
         }
-        System.out.println(coach.getFortuneService());
+        boolean equals = (coach == alphaCoach);
+        System.out.println("Links are the same: " + equals);
+        System.out.println("coach: " + coach);
+        System.out.println("alphaCoach: " + alphaCoach);
+
     }
 }
