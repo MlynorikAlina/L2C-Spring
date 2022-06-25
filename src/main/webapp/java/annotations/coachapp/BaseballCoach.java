5a6f92831e30436a49406cc2e3435c9ecac0bba0
Alina Mlynorik <alina.sudnick.@gmail.com>
2022-06-25 12:28:44 +0300

    coachapp-update-5: bean scope

--- src/main/webapp/java/annotations/coachapp/BaseballCoach.java
+++ src/main/webapp/java/annotations/coachapp/BaseballCoach.java
@@ -2,9 +2,11 @@ package annotations.coachapp;
 
 import annotations.coachapp.service.FortuneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class BaseballCoach extends Coach {
 
     @Override
