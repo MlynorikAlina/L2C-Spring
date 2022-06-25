5ccc82d123e3fb72cac35f169369fd2e91af782a
Alina Mlynorik <alina.sudnick.@gmail.com>
2022-06-25 12:07:50 +0300

    coachapp-update-4: awtowired, value injection

--- src/main/webapp/java/annotations/coachapp/Coach.java
+++ src/main/webapp/java/annotations/coachapp/Coach.java
@@ -3,11 +3,14 @@ package annotations.coachapp;
 import annotations.coachapp.service.FortuneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 
 public abstract class Coach {
     @Autowired
     @Qualifier("foo")
     private FortuneService fortuneService;
+    @Value("${foo.team}")
+    private String team;
 
     public abstract String getDailyWorkout();
     
@@ -15,4 +18,7 @@ public abstract class Coach {
         return fortuneService.getFortune();
     }
 
+    public String getTeam() {
+        return team;
+    }
 }
